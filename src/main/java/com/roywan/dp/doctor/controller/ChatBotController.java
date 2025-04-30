033ed24766a72283de5d5cd1a7fdb6e82d186e07
wawo00 <uruz3080@gmail.com>
2025-04-30 15:53:07 +0800

    【流式输出的测试用例】

--- src/main/java/com/roywan/dp/doctor/controller/ChatBotController.java
+++ src/main/java/com/roywan/dp/doctor/controller/ChatBotController.java
@@ -1,10 +1,14 @@
 package com.roywan.dp.doctor.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.awt.*;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -22,4 +26,24 @@ public class ChatBotController {
     public String chat(@PathVariable("message") String msg) {
         return chatClient.prompt().user(msg).call().content();
     }
+
+    @GetMapping(value = "/stream",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter streamData(){
+        //创建发射器，超时时间1分钟
+        SseEmitter emitter=new SseEmitter(60*1000L);
+        new Thread(()->{
+            try{
+                for (int i = 0; i < 6; i++) {
+                    Thread.sleep(1000);
+                    emitter.send("time="+System.currentTimeMillis());
+                }
+
+            }catch (Exception e){
+                emitter.completeWithError(e);
+            }
+
+        }).start();
+        return emitter;
+    }
+
 }
